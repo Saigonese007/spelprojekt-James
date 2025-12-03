73a7ae51b5bb96c35d7c5dbcafa81e0b01fa4d0f
Saigonese07 <musicjames08@gmail.com>
2025-12-03 10:49:07 +0100

    Webbutvecklings lektion 12/03
    2025-12-03
    
    Lagt till:
    Gravitation
    Max height
    Max bottom
    gamover text
    
    Fixade:
    Scoreposition
    Base
    Animation så att den är based på gravitation istället för space bar

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.ComponentModel.DataAnnotations;
 
 namespace spelprojekt_James
 {
@@ -17,12 +18,28 @@ namespace spelprojekt_James
         KeyboardState oldkeyinput;
 
         Rectangle backgroundrec = new Rectangle(0, 0, 540, 960);
+        Rectangle deathbaserec = new Rectangle(0, 960 - 200, 540, 200); // 960 - 200 pga line up med bottom 
 
+        //Text
         SpriteFont Font;
-        string displaytext;
+        string displaytext; //ska ändras
         int score = 0;
-        Vector2 scorePosition = new Vector2(400 / 2, 100);
+        Vector2 scorePosition = new Vector2(540 / 2, 100);
         string gameoverText = "Game Over";
+
+        //Gravitation
+        //float istället för vector2 pga bara Y-led ska röra sig
+        float gravity = 0.4f; // acceleration nedåt varje frame
+        float velocity = 0; //hur snabbt fågeln rör sig upp eller ner (0 pga att den står stilla när spelet börjar)
+        float flapStyrka = -6f; // hur högt/starkt fågeln ska hoppa när man trycker på space (-6 kändes lagom högt)
+
+
+        // max heights
+        int MaxHeightBottom = 960 - 250; // deathbase ska döda
+        int MaxHeightTop = 0;
+
+        //Gamover
+        bool GameOver = false;
 
         public Game1()
         {
@@ -50,6 +67,8 @@ namespace spelprojekt_James
             birddown = Content.Load<Texture2D>("yellowbird-downflap");
             bird = Content.Load<Texture2D>("yellowbird-midflap");
 
+            deathbase = Content.Load<Texture2D>("base");
+
 
             Font = Content.Load<SpriteFont>("Font");
 
@@ -59,6 +78,54 @@ namespace spelprojekt_James
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            oldkeyinput = keyinput;
+            keyinput = Keyboard.GetState();
+
+            //junping mech
+            if (keyinput.IsKeyDown(Keys.Space) && oldkeyinput.IsKeyUp(Keys.Space))
+            {
+                velocity = flapStyrka; // skickar velocity till flapStyrka så att fågeln hoppar upp
+            
+            }
+
+            velocity += gravity; // om man inte hoppar så åker man neråt med gravitationen
+
+            birdrec.Y += (int)velocity; // flyttar fågeln till antingen gravitation eller upp med flapStryka, omvandla float till int
+
+            //animation
+            if (velocity < 0)
+            {
+                bird = birdup; // FlapUp när Velocity är större än 0
+            }
+            else if (velocity > 0)
+            {
+                bird = birddown; //Vingar ner när velocity är mindre än 0
+            }
+            else
+            {
+                bird = bird; // natural position
+            }
+
+            // Hamnar man utanför = GameOver
+            if (birdrec.Y <= MaxHeightTop || birdrec.Y >= MaxHeightBottom)
+            {
+                GameOver = true;
+                score = 0; //reset score
+                velocity = 0;
+            }
+
+            //GameOver Actions
+            if (GameOver)
+            {
+                displaytext = gameoverText;
+            }
+            else
+            {
+                displaytext = score.ToString();
+            }
+
+
 
 
 
@@ -71,6 +138,7 @@ namespace spelprojekt_James
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(background, backgroundrec, Color.White);
+            _spriteBatch.Draw(deathbase, deathbaserec, Color.White);
             _spriteBatch.Draw(bird, birdrec, Color.White);
             _spriteBatch.DrawString(Font, displaytext, scorePosition, Color.White);
             _spriteBatch.End();
